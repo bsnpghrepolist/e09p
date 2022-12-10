819701c817ef78640ddc018b476cdd6866f9cb25
root <root@ip-172-31-87-112.ec2.internal>
2022-12-10 19:27:17 +0000

    do git_add_commit_push_Sync(true).

--- account/dat/MostRecentVerses_json.js
+++ account/dat/MostRecentVerses_json.js
@@ -15,15 +15,7 @@ var MostRecentVerses_json =
         "MostRecentSearch": [
             {
                 "[\"glory\",0,\"NIV\",\"Genesis\"]": "221210 192628",
-                "[\"live by\",29,\"NIV\",\"all\"]": "221104 183630",
-                "[\"live by\",29,\"NIV\",\"\"]": "221103 230631",
-                "[\"live by\",0,\"NIV\",\"Amos\"]": "221103 230621",
                 "[\"live by\",10,\"NIV\",\"Paulines\"]": "221014 110403",
-                "[\"live by\",15,\"ESV\",\"all\"]": "221014 025638",
-                "[\"life\",526,\"ESV\",\"all\"]": "221014 022258",
-                "[\"life\",18,\"NIV\",\"Matthew\"]": "221014 000233",
-                "[\"glory\",3,\"NIV\",\"Matthew\"]": "221014 000050",
-                "[\"life\",575,\"NIV\",\"all\"]": "221013 235845",
                 "life": "221013 180635,19, NIV, UserDef: Mal Mat",
                 "Jesus": "221013 175441,209 found in 'NIV' in 'Mal Mat'.",
                 "粪土": "221013 174620,1 found in 'NT' of 'CUVS'.",
