a2a97b9e3a4acf894a0f33ad4a39ff5a49af7bff
root <root@ip-172-31-87-112.ec2.internal>
2022-12-10 17:20:51 +0000

    do git_add_commit_push_Sync(true).

---
+++ account/dat/MostRecentVerses_json.js
@@ -0,0 +1,14 @@
+var MostRecentVerses_json = 
+{
+    "MostRecent_Verses": {
+        "RecentAccessed": [
+            {
+                "Gen1:1": "221210 172010",
+                "Gen1:4": "221210 100514",
+                "Gen1:6": "221210 100306",
+                "Oba1:1": "221107 233928",
+                "Gen1:2": "221106 192246"
+            }
+        ]
+    }
+}
